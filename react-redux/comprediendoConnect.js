d78b2fb7aebef736ef88dc26f60a9f991005c0d7
yirleison <yppalomeque@gmail.com>
2019-12-09 22:33:48 -0500

    Se inicia documentacion acerca de redux,Provider y connect

--- react-redux/comprediendoConnect.js
+++ react-redux/comprediendoConnect.js
@@ -1,16 +1,14 @@
 /**
  * Este tipo de funcion se denomina funcion de orden superior, es una funcion la cual retorna otra función
  * y la función retornada puede o no recibir parámetros.
- * @param {*} func1 
- * @param {*} func2 
+ * @param {*} value1 
+ * @param {*} values2 
  */
-const connect = (func1, func2) => {
-    const dato1 = func1();
-    console.log(dato1);
-    console.log(func1);
-    console.log(func2);
+const connect = (value1, values2) => {
+    console.log(value1);
+    console.log(values2);
     return (component) => {
-        const result = `${component}-${func1()*func2()}`;
+        const result = `${component}-${value1()*values2()}`;
         console.log(result);
         return result;
     }
